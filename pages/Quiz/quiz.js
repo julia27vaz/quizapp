1386d4fa6f3d468eeef961c89cbbc0aa52a6e7fc
julia khiara <juliakhiara@gmail.com>
2024-06-20 02:37:50 +0000

    validar resposta

--- pages/Quiz/quiz.js
+++ pages/Quiz/quiz.js
@@ -9,6 +9,7 @@ let pontos = 0
 let pergunta = 1
 let resposta = ""
 let idInputResposta = ""
+let respostaCorretaId= ""
 
 botaoTema.addEventListener("click", () => {
     trocarTema(body, botaoTema)
@@ -100,6 +101,20 @@ function guardarResposta(evento) {
     idInputResposta = evento.target.id
 }
 
+const botaoEnviar = document.querySelector(".alternativas button")
+botaoEnviar.addEventListener("click", validarResposta)
+}
+
+function validarResposta() {
+if (resposta === quiz.questions[pergunta-1].answer) {
+    document.querySelector(`label[for="${idInputResposta}"]`).setAttribute("id", "correta")
+    pontos = pontos + 1
+} else {
+    document.querySelector(`label[for="${idInputResposta}"]`).setAttribute("id", "errada")
+    console.log(respostaCorretaId)
+    document.querySelector(`label[for="${respostaCorretaId}"]`).setAttribute("id", "correta")
+}
+
 
 
 async function iniciar() {
@@ -111,6 +126,10 @@ async function iniciar() {
 const inputsResposta = document.querySelectorAll(".alternativas input")
     inputsResposta.forEach(input => {
         input.addEventListener("click", guardarResposta)
+
+        if(input.value === quiz.questions[pergunta-1].answer) {
+            respostaCorretaId = input.id
+        }
     })
 
 iniciar()
